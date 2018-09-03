acee4cbe5f607e01dff638e15679408a1c32a7d3
Elvin Asadov <elvin_asadov@epam.com>
2018-09-03 11:54:23 +0300

    Add project files.

---
+++ SimLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
